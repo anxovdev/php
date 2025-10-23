3dd1e2076c353ad06b2405917d50c680417bb826
anxov.dev <anxov.dev@gmail.com>
2025-10-23 06:57:53 -0400

    Avance SQL

---
+++ mySQL/04-SQLDirectQuery.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "wuser";
+$password = "abc123.";
+$myDB = "world";
+try {
+  //1 open
+  $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+
+
+  // 2 Process
+  $sql = "SELECT * FROM `city` where CountryCode = (SELECT Code from country where Name = 'Spain')";
+  $conn->query($sql);
+
+  $results = $conn->query($sql);
+  foreach($results as $row) {
+    echo $row['Name'] . " " . $row['Population'] . "<br>";
+  }
+
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+  //3 close
+  $conn = null;
+?>
